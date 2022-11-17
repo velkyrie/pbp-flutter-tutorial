4a143d649629543bbf8ab1bfc874859402571861
jinggamentari <vladijm@gmail.com>
2022-11-17 16:05:39 +0700

    done tutorial 8

--- tutorial/lib/page/form.dart
+++ tutorial/lib/page/form.dart
@@ -1,5 +1,6 @@
 import 'package:tutorial/main.dart';
 import 'package:flutter/material.dart';
+import 'package:tutorial/page/to_do_page.dart';
 
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
@@ -50,6 +51,16 @@ class _MyFormPageState extends State<MyFormPage> {
                 );
               },
             ),
+            ListTile(
+            title: const Text('To Do'),
+            onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ToDoPage()),
+                );
+            },
+        ),
           ],
         ),
       ),
